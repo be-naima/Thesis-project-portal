ed5bb2336de4bbadf7e38da4d92ea170f1a2436b
be-naima <naimafardous03@gmail.com>
2024-10-24 21:35:24 -0700

    announcement

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../../assets/custom css/custom.css';
+import { Slide } from 'react-awesome-reveal';
 
 const Banner = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
@@ -20,7 +21,7 @@ const Banner = () => {
     }, [slides.length]);
 
     return (
-        <div>
+        <div className='mb-44'>
             <div className="flex flex-col sm:flex-row w-full h-screen">
 
                 <div className="relative w-full sm:w-1/2 h-1/2 sm:h-full overflow-hidden">
@@ -36,42 +37,42 @@ const Banner = () => {
 
 
                 <div className="flex flex-col items-center justify-center w-full sm:w-1/2 h-1/2 sm:h-full text-center bannerColor text-indigo-700 p-8">
-                    <h1 className="text-3xl custom-heading font-extrabold sm:text-4xl md:text-5xl lg:text-5xl animate-slideInRight">
+                   <Slide direction="right" > <h1 className="text-3xl custom-heading font-extrabold sm:text-4xl md:text-5xl lg:text-5xl animate-slideInRight">
                         THESIS PROJECT HUB
-                    </h1>
-                    <h4 className="mt-4 text-bold sm:text-base md:text-lg lg:text-xl sub-heading animate-slideInRight">
+                    </h1></Slide>
+                    <Slide direction="right" ><h4 className="mt-4 text-bold sm:text-base md:text-lg lg:text-xl sub-heading animate-slideInRight">
                         Effortless Submission for Your <br /> Academic Research
-                    </h4>
-                    <div className="mt-8 flex space-x-4">
-                        <button className="px-6 py-2 font-semibold text-white bg-indigo-600 rounded-md hover:bg-indigo-800 animate-slideInRight">
+                    </h4></Slide>
+                    <Slide direction="right" > <div className="mt-8 flex space-x-4">
+                    <button className="px-6 py-2 font-semibold text-white bg-indigo-600 rounded-md hover:bg-indigo-800 animate-slideInRight">
                             Explore Thesis/Projects
                         </button>
                         <button className="px-6 py-2 font-semibold text-white bg-green-700 rounded-md hover:bg-green-800 animate-slideInRight">
                             Submit Your Thesis/Projects
                         </button>
-                    </div>
+                    </div> </Slide>
                 </div>
             </div>
 
 
             <div className="relative z-20 flex flex-col items-center justify-center mt-10 space-y-4 bg-transparent sm:flex-row sm:space-x-4 sm:space-y-0 sm:-mt-16">
 
-                <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg text-center h-72 sm:h-80 border-t-8 border-indigo-500 animate-slideInRight">
+            <Slide direction="left" > <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg text-center h-72 sm:h-80 border-t-8 border-indigo-500 animate-slideInRight">
                     <h3 className="text-xl font-bold">Why Choose Our Thesis Portal</h3>
                     <p className="mt-2 text-gray-600">Streamline your research journey with our easy-to-use platform. Whether you’re uploading, reviewing, or collaborating on thesis projects, our portal is designed to help students and faculty manage academic research with ease and efficiency.</p>
-                </div>
+                </div> </Slide>
 
 
-                <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg text-center h-72 sm:h-80 border-t-8 border-indigo-500 animate-slideInBottom">
+                <Slide direction="right" ><div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg text-center h-72 sm:h-80 border-t-8 border-indigo-500 animate-slideInBottom">
                     <h3 className="text-xl font-bold">Thesis Guidelines</h3>
                     <p className="mt-2 text-gray-600">Access comprehensive thesis submission guidelines, format instructions, and best practices. Ensure your thesis meets the university’s standards with clear, concise directions and resources.</p>
-                </div>
+                </div></Slide>
 
 
-                <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg text-center h-72 sm:h-80 border-t-8 border-indigo-500 animate-slideInRight">
+                <Slide direction="right" ><div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-lg text-center h-72 sm:h-80 border-t-8 border-indigo-500 animate-slideInRight">
                     <h3 className="text-xl font-bold">Start Your Submission</h3>
                     <p className="mt-2 text-gray-600">Ready to submit your thesis? Use our secure and intuitive submission process to upload your work, track its progress, and receive feedback from advisors and reviewers.</p>
-                </div>
+                </div></Slide>
             </div>
         </div>
     );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Announcement from "./Announcement";
 import Banner from "./Banner";
 
 
@@ -5,6 +6,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Announcement></Announcement>
         </div>
     );
 };
